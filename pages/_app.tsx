4b7f67fddf8950906b89dfbaaceff74f7b8d517c
Yurii M <mokruy9999@gmail.com>
2026-03-15 19:09:43 +0100

    task(feat): restore favicon, VPC-1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import '../src/assets/main.css'
+import Head from 'next/head'
 import i18n from '../src/i18n'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
@@ -17,5 +18,14 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   }, [pageProps.lang])
 
   console.log('MyApp rendering with lang:', pageProps.lang)
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
